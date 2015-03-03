c645d5c25fd46b477c873061e26144312da1f7f4
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-03 10:15:27 +0300

    referee tests

---
+++ verification/src/tests.py
@@ -0,0 +1,76 @@
+TESTS = {
+    "Level_1": [
+        {
+            "input": "02:30",
+            "answer": 105.0
+        },
+        {
+            "input": "18:00",
+            "answer": 180.0
+        },
+        {
+            "input": "12:01",
+            "answer": 5.5
+        },
+        {
+            "input": "00:00",
+            "answer": 0.0
+        },
+        {
+            "input": "01:43",
+            "answer": 153.5
+        },
+        {
+            "input": "01:42",
+            "answer": 159.0
+        },
+        {
+            "input": "13:42",
+            "answer": 159.0
+        },
+        {
+            "input": "23:59",
+            "answer": 5.5
+        },
+        {
+            "input": "05:40",
+            "answer": 70.0
+        },
+        {
+            "input": "05:16",
+            "answer": 62.0
+        },
+        {
+            "input": "20:37",
+            "answer": 36.5
+        },
+        {
+            "input": "00:23",
+            "answer": 126.5
+        },
+        {
+            "input": "23:47",
+            "answer": 71.5
+        },
+        {
+            "input": "07:01",
+            "answer": 155.5
+        },
+        {
+            "input": "13:50",
+            "answer": 115.0
+        },
+        {
+            "input": "19:27",
+            "answer": 61.5
+        },
+        {
+            "input": "04:03",
+            "answer": 103.5
+        },
+        {
+            "input": "14:55",
+            "answer": 117.5
+        },
+    ]
+}
