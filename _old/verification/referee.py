d32b0a03822d66bf4f53e082fa8e50ac4f0f90ee
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-25 10:40:33 +0300

    refactors

--- _old/verification/referee.py
+++ _old/verification/referee.py
@@ -46,7 +46,7 @@ api.add_listener(
             'python-27': cover,
             'python-3': cover
         },
-        function_name="clock_angle",
+        DEFAULT_FUNCTION_NAME="clock_angle",
         checker=checkers.float_comparison(1)
         # add_allowed_modules=[],
         # add_close_builtins=[],
